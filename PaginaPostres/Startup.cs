db9514f5baf024cccd8b094dac6680dbc0f8d6ae
paularamos18 <paula.ramos18@tectijuana.edu.mx>
2020-12-03 21:06:26 -0800

    Inicio2

---
+++ PaginaPostres/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PaginaPostres.Startup))]
+namespace PaginaPostres
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
